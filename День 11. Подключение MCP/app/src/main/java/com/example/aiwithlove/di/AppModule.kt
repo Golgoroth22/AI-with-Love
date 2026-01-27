d5f77950e037ffa4235375451436ce68f91b94a4
Valentin Falin <falinv22@gmail.com>
2026-01-27 16:33:59 +0700

    [day11] Init commit

---
+++ День 11. Подключение MCP/app/src/main/java/com/example/aiwithlove/di/AppModule.kt
@@ -0,0 +1,33 @@
+package com.example.aiwithlove.di
+
+import com.example.aiwithlove.data.PerplexityApiService
+import com.example.aiwithlove.data.PerplexityApiServiceImpl
+import com.example.aiwithlove.database.AppDatabase
+import com.example.aiwithlove.database.ChatMessageDao
+import com.example.aiwithlove.database.ChatRepository
+import com.example.aiwithlove.util.SecureData
+import com.example.aiwithlove.viewmodel.ChatViewModel
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.dsl.module
+
+val appModule =
+    module {
+        single<PerplexityApiService?> {
+            PerplexityApiServiceImpl(SecureData.apiKey)
+        }
+
+        single<AppDatabase> {
+            AppDatabase.getDatabase(androidContext())
+        }
+
+        single<ChatMessageDao> {
+            get<AppDatabase>().chatMessageDao()
+        }
+
+        single<ChatRepository> {
+            ChatRepository(get())
+        }
+
+        viewModelOf(::ChatViewModel)
+    }
